2e26a225b5bae2c55c12391b51d4570b0f6380cf
SIDDHANT MORE <siddhantmore9167@gmail.com>
2020-12-07 18:42:25 +0530

    React Redux Form Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Label,  Col, Row } from 'reactstrap';
 import {Link} from 'react-router-dom';
-import {Control, LocalForm, Errors } from 'react-redux-form';
+import {Control, Form, Errors, actions } from 'react-redux-form';
 
 
 
@@ -28,7 +28,8 @@ class Contact extends Component {
     handleSubmit(values)
     {
         
-        alert(JSON.stringify(values))
+        alert(JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
    
@@ -81,7 +82,7 @@ class Contact extends Component {
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className = "col-12 col-md-9">
-                        <LocalForm onSubmit = {(values) => this.handleSubmit(values)}>
+                        <Form model = 'feedback' onSubmit = {(values) => this.handleSubmit(values)}>
                         <Row className="form-group">
                             <Label htmlFor='firstname' md={2}>First Name</Label>
                             <Col md={10}>
@@ -196,7 +197,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import DishDetail from './DishDetailComponent';
 import About from './AboutComponent';
 import {connect } from 'react-redux';
 import {addComment, fetchDishes} from '../redux/ActionCreators';
-
+import {actions} from 'react-redux-form';
 
 const mapStateToProps = state =>{
 
@@ -26,7 +26,8 @@ const mapStateToProps = state =>{
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
 });
 
 
@@ -78,7 +79,7 @@ class Main extends Component {
             <Route exact path='/aboutus' component={AboutPage} />
             <Route exact path='/menu' component={() => <Menu dishes = {this.props.dishes}/>}/>
             <Route path="/menu/:dishId" component = {DishWithId }/>
-            <Route exact path='/contactus' component={Contact} />
+            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm} />} />
             <Redirect to='/home'/>
         </Switch>
         <Footer/>
